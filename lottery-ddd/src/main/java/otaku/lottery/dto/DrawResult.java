07c4d68f620532d151f8d13cd1d9a339472bb321
blueSky <215841796@qq.com>
2021-10-10 22:05:49 +0800

    Delete lottery-ddd directory

--- lottery-ddd/src/main/java/otaku/lottery/dto/DrawResult.java
+++
@@ -1,9 +0,0 @@
-package otaku.lottery.dto;
-
-import lombok.Data;
-
-@Data
-public class DrawResult {
-    private Long recordId;
-    private String award;
-}
--- lottery-ddd/src/main/java/otaku/lottery/service/package-info.java
+++
@@ -1,3 +0,0 @@
-package otaku.lottery.service;
-
- // service组合entity和repository，对外提供接口
